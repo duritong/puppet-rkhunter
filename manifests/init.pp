eba275cb49ef8572f036c74a9e5eef832c2891b6
mh <mh@immerda.ch>
2009-01-07 09:07:48 +0000

    manage as well sysconfig file

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,11 +2,10 @@
 # Copyright (C) 2007 admin@immerda.ch
 # 
 
-#modules_dir { "rkhunter": }
-
 class rkhunter {
     case $operatingsystem {
         gentoo: { include rkhunter::gentoo }
+        centos: { include rkhunter::centos }
         default: { include rkhunter::base }
     }
 
@@ -18,24 +17,34 @@ class rkhunter::base {
     }
 
     file {"/etc/rkhunter.conf":
-            source => [ "puppet://$server/files/rkhunter/${fqdn}/rkhunter.conf",
-                        "puppet://$server/files/rkhunter/${operatingsystem}/${lsbdistcodename}/rkhunter.conf",
-                        "puppet://$server/files/rkhunter/${operatingsystem}/rkhunter.conf",
-                        "puppet://$server/files/rkhunter/rkhunter.conf",
-                        "puppet://$server/rkhunter/${operatingsystem}/${lsbdistcodename}/rkhunter.conf",
-                        "puppet://$server/rkhunter/${operatingsystem}/rkhunter.conf",
-                        "puppet://$server/rkhunter/rkhunter.conf" ],
-	        require => Package['rkhunter'],
-            mode => 0400, owner => root, group => root;
-     }
+        source => [ "puppet://$server/files/rkhunter/${fqdn}/rkhunter.conf",
+                    "puppet://$server/files/rkhunter/${operatingsystem}/${lsbdistcodename}/rkhunter.conf",
+                    "puppet://$server/files/rkhunter/${operatingsystem}/rkhunter.conf",
+                    "puppet://$server/files/rkhunter/rkhunter.conf",
+                    "puppet://$server/rkhunter/${operatingsystem}/${lsbdistcodename}/rkhunter.conf",
+                    "puppet://$server/rkhunter/${operatingsystem}/rkhunter.conf",
+                    "puppet://$server/rkhunter/rkhunter.conf" ],
+        require => Package['rkhunter'],
+        mode => 0400, owner => root, group => root;
+    }
     case $operatingsystem {
         debian: {
             case $lsbdistcodename {
-                'etch': { info("no need to ini the database") }
+                'etch': { info("no need to init the database") }
                 default: { include rkhunter::dbinit }
             }
         }
         default: { include rkhunter::dbinit }
+    }
+}
+
+class rkhunter::centos inherits rkhunter::base {
+    file{'/etc/sysconfig/rkhunter':
+        source => [ "puppet://$server/files/rkhunter/sysconfig/${fqdn}/rkhunter",
+                    "puppet://$server/files/rkhunter/sysconfig/rkhunter",
+                    "puppet://$server/rkhunter/sysconfig/rkhunter" ],
+        require => Package['rkhunter'],
+        mode => 0644, owner => root, group => root;
     }
 }
 
