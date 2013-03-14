c5900bdb74d2eb1bc5f6883626ee3a5540805549
mh <mh@immerda.ch>
2013-03-14 23:51:44 +0100

    fix typo

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,6 +1,6 @@
 # manage centos
 class rkhunter::centos inherits rkhunter::base {
-  file_lin{'rkhunter_mail_root':
+  file_line{'rkhunter_mail_root':
     path    => '/etc/sysconfig/rkhunter',
     line    => 'MAILTO=root',
     match   => '^MAILTO=',
