887f88e77515c4cb31a93d077597d1e39c14ef18
Justice London <jlondon@syrussystems.com>
2013-04-16 09:43:16 -0700

    Fixed missing path statement for db update.

--- manifests/dbinit.pp
+++ manifests/dbinit.pp
@@ -1,7 +1,8 @@
 class rkhunter::dbinit {
-    exec{'init_rkunter_db':
-        command => 'rkhunter --propupd',
-        creates => '/var/lib/rkhunter/db/rkhunter.dat',
-        require => Package['rkhunter'],
-    }
+  exec{'init_rkunter_db':
+    command => 'rkhunter --propupd',
+    creates => '/var/lib/rkhunter/db/rkhunter.dat',
+    require => Package['rkhunter'],
+    path    => ['/sbin','/bin','/usr/bin','/usr/sbin'],
+  }
 }
