232601a2aae406f0b79dc6839a8cf4b1313d909c
mh <mh@immerda.ch>
2015-10-11 14:34:41 +0200

    make the local a concat file so other modules can contribute to it

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,15 +1,22 @@
 # manage basic installation
 class rkhunter::base {
   package { 'rkhunter':
-    ensure => present,
-  } -> file{'/etc/rkhunter.conf.local': # all user specific changes can go here
-    content => template('rkhunter/rkhunter.conf.local.erb'),
+    ensure => installed,
+  }
+  # all user specific changes can go here
+  concat{'/etc/rkhunter.conf.local':
     owner   => root,
     group   => 0,
-    mode    => '0400';
+    mode    => '0400',
+    require => Package['rkhunter'],
+  }
+  concat::fragment{'module_default':
+    target  => '/etc/rkhunter.conf.local',
+    content => template('rkhunter/rkhunter.conf.local.erb'),
+    order   => '05',
   }
 
-  include rkhunter::dbinit
+  include ::rkhunter::dbinit
   ensure_packages('prelink')
   file{'/usr/local/sbin/rkhunter_prelinker':
     source  => 'puppet:///modules/rkhunter/prelinker.rb',
