48237bcef86eb2f5d500251e8b5a8e4d692f7ba7
mh <mh@immerda.ch>
2017-07-26 20:55:08 +0200

    fix reports about unknown prelink hash on EL6 - https://sourceforge.net/p/rkhunter/bugs/156/

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -6,4 +6,10 @@ class rkhunter::centos inherits rkhunter::base {
     match   => '^MAILTO=',
     require => Package['rkhunter'],
   }
+  if versioncmp($::operatingsystemmajrelease,'7') < 0 {
+    concat::fragment{'HASH_CMD':
+      target  => '/etc/rkhunter.conf.local',
+      content => 'HASH_CMD=sha1sum',
+    }
+  }
 }
